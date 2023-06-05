e186a9691b06da8a1b150aa2ce6b6d5f2d66654e
liamhandasyde <lightspace@me.com>
2023-06-05 23:11:02 +1000

    Added comments. Also never used Mac Visual Studio’s git changes

--- FhirUtility/Program.cs
+++ FhirUtility/Program.cs
@@ -1,15 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Common;
+﻿using Common;
 using Common.Extensions;
 using Hl7.Fhir.Model;
 using Microsoft.Extensions.Configuration;
 
+//  Register access to User Secrets.
 var environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
 var builder = new ConfigurationBuilder()
             .AddUserSecrets<Program>();
             
 var configurationRoot = builder.Build();
 
+//  Extract User Secrets and warn if they don't exist.
 var baseUrl = configurationRoot.GetSection("Fhir:Base_Url")?.Value;
 var username = configurationRoot.GetSection("Fhir:username")?.Value;
 var password = configurationRoot.GetSection("Fhir:password")?.Value;
@@ -24,29 +25,38 @@ if (string.IsNullOrEmpty(baseUrl)
     return;
 }
 
+//  Read the JSON file for all required custom Search Parameters to a bundle. 
 var bundle = await path.ReadDataFileAsync();
 
+//  Cast to a List of type SearchParameter.
 var searchParameters = bundle.ToFhirResourceWherePosible<SearchParameter>();
 
+//  Comma delimited string of Id for the Fhir Service query. This is MVP. There may come a time when there are too many and a batching strategy will be required.
 var ids = string.Join(',', searchParameters
     .Select(p => p.Id)
     .ToList());
 
 var httpClient = HttpExtensions.CreateHttpClient(baseUrl, username, password);
+
+//  Execute the request.
 var content = await httpClient.GetRequestAsync($"{typeof(SearchParameter).Name}?_id={ids}");
 
+//  Cast the response back to a List of type SearchParameter.
 var response = content
     .ToBundle()
     .ToFhirResourceWherePosible<SearchParameter>();
 
+//  Calculate missing SearchParameter/s.
 var missingSearchParameters = response
     .Where(p => ids.Contains(p.Id))
     .ToList();
 
+//  Write results.
 Console.WriteLine($"Environment: {baseUrl}\n" +
     $"Custom {typeof(SearchParameter).Name}'s: {searchParameters.Count}\n" +
     $"Missing {typeof(SearchParameter).Name}'s: {missingSearchParameters.Count}\n\n");
 
+//  Write individual missing SearchParameter/s.
 var i = 1;
 foreach (var searchParameter in missingSearchParameters)
 {
@@ -54,6 +64,7 @@ foreach (var searchParameter in missingSearchParameters)
     i++;
 }
 
+//  Done.
 Console.WriteLine("\n\nFhir Utility completed");
 Console.ReadKey();
 
