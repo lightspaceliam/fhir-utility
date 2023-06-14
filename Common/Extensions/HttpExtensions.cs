86a99bff1c3679ccc05978cec6bb41097afbaa1e
Liam Handasyde <liam.handasyde@health.telstra.com>
2023-06-14 11:20:46 +1000

    add post put and delete

--- Common/Extensions/HttpExtensions.cs
+++ Common/Extensions/HttpExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 
@@ -31,8 +32,74 @@ namespace Common.Extensions
                 var content = await httpResponseMessage.Content.ReadAsStringAsync();
                 if (!httpResponseMessage.IsSuccessStatusCode)
                 {
+                    if(httpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound) 
+                    {
+                        return "";
+                    }
+
                     throw new Exception($"Something went wrong: {httpResponseMessage.StatusCode}");
                 }
+                return content;
+            }
+        }
+
+        public static async Task<string> PostRequestAsync(HttpClient httpClient, string uri, StringContent stringContent)
+        {
+            using (var httpResponseMessage = await httpClient.PostAsync(uri, stringContent))
+            {
+                var content = await httpResponseMessage.Content.ReadAsStringAsync();
+                if (!httpResponseMessage.IsSuccessStatusCode)
+                {
+                    if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        return "";
+                    }
+
+                    throw new Exception($"Something went wrong: {httpResponseMessage.StatusCode}");
+                }
+
+                return content;
+            }
+        }
+
+        public static async Task<string> PutRequestAsync(HttpClient httpClient, string uri, StringContent stringContent, AuthenticationHeaderValue authenticationHeaderValue)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = authenticationHeaderValue;
+
+            using (var httpResponseMessage = await httpClient.PutAsync(uri, stringContent))
+            {
+                var content = await httpResponseMessage.Content.ReadAsStringAsync();
+                if (!httpResponseMessage.IsSuccessStatusCode)
+                {
+                    if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        return "";
+                    }
+
+                    throw new Exception($"Something went wrong: {httpResponseMessage.StatusCode}");
+                }
+
+                return content;
+            }
+        }
+
+        public static async Task<string> DeleteAsync(HttpClient httpClient, string uri, AuthenticationHeaderValue authenticationHeaderValue)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = authenticationHeaderValue;
+
+            using (var httpResponseMessage = await httpClient.DeleteAsync(uri))
+            {
+                var content = await httpResponseMessage.Content.ReadAsStringAsync();
+                if (!httpResponseMessage.IsSuccessStatusCode)
+                {
+                    if (httpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        return "";
+                    }
+
+                    throw new Exception($"Something went wrong: {httpResponseMessage.StatusCode}");
+                }
+
                 return content;
             }
         }
