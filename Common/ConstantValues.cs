154be83ad0a7cdfe91b0555c854592571ec3a171
Liam Handasyde <liam.handasyde@health.telstra.com>
2023-06-14 11:21:25 +1000

    mvp querying detected issue

---
+++ Common/ConstantValues.cs
@@ -0,0 +1,26 @@
+﻿namespace Common
+{
+    public static class ConstantValues
+    {
+        private const string FHIR_RESOURCE_BASE = "api/resource/";
+
+        public readonly static string FHIR_RESOURCE_PATIENT_AREA = $"{FHIR_RESOURCE_BASE}patients";
+        public readonly static string FHIR_RESOURCE_EPISODE_OF_CARE_AREA = $"{FHIR_RESOURCE_BASE}episodeofcare";
+        public readonly static string FHIR_RESOURCE_CARE_PLAN_AREA = $"{FHIR_RESOURCE_BASE}care-plan";
+        public readonly static string FHIR_RESOURCE_GROUPS_AREA = $"{FHIR_RESOURCE_BASE}groups";
+        public readonly static string FHIR_RESOURCE_CARE_TEAM_AREA = $"{FHIR_RESOURCE_BASE}care-teams";
+        public readonly static string FHIR_RESOURCE_TASK_AREA = $"{FHIR_RESOURCE_BASE}task";
+        public readonly static string FHIR_RESOURCE_CONDITION_AREA = $"{FHIR_RESOURCE_BASE}conditions";
+        public readonly static string FHIR_RESOURCE_PRACTITIONER_AREA = $"{FHIR_RESOURCE_BASE}practitioners";
+        public readonly static string FHIR_RESOURCE_DETECTED_ISSUE_AREA = $"{FHIR_RESOURCE_BASE}detected-issues";
+        public readonly static string FHIR_RESOURCE_DOCUMENT_REFERENCE_AREA = $"{FHIR_RESOURCE_BASE}document-references";
+        public readonly static string FHIR_RESOURCE_OBSERVATION_AREA = $"{FHIR_RESOURCE_BASE}observation";
+        public readonly static string FHIR_RESOURCE_ORGANIZATION_AREA = $"{FHIR_RESOURCE_BASE}organizations";
+        public readonly static string FHIR_RESOURCE_PLAN_DEFINITION_AREA = $"{FHIR_RESOURCE_BASE}plan-definitions";
+        public readonly static string FHIR_RESOURCE_PRACTITIONER_ROLE_AREA = $"{FHIR_RESOURCE_BASE}practitioner-roles";
+        public readonly static string FHIR_RESOURCE_QUESTIONNAIRE_AREA = $"{FHIR_RESOURCE_BASE}questionnaire";
+        public readonly static string FHIR_RESOURCE_RELATED_PEOPLE_AREA = $"{FHIR_RESOURCE_BASE}related-people";
+        public readonly static string FHIR_RESOURCE_TRIAGE_DASHBOARD_AREA = $"{FHIR_RESOURCE_BASE}triage-dashboard";
+        public readonly static string FHIR_RESOURCE_FLAG_AREA = $"{FHIR_RESOURCE_BASE}flag";
+    }
+}
--- FhirUtility/Program.cs
+++ FhirUtility/Program.cs
@@ -24,45 +24,106 @@ if (string.IsNullOrEmpty(baseUrl)
     Console.WriteLine("Please read the readme for required user secrets properties");
     return;
 }
-
-//  Read the JSON file for all required custom Search Parameters to a bundle. 
-var bundle = await path.ReadDataFileAsync();
-
-//  Cast to a List of type SearchParameter.
-var searchParameters = bundle.ToFhirResourceWherePosible<SearchParameter>();
-
-//  Comma delimited string of Id for the Fhir Service query. This is MVP. There may come a time when there are too many and a batching strategy will be required.
-var ids = string.Join(',', searchParameters
-    .Select(p => p.Id)
-    .ToList());
-
 var httpClient = HttpExtensions.CreateHttpClient(baseUrl, username, password);
+var runSearchParams = false;
+var runBulkDataOp = true;
 
-//  Execute the request.
-var content = await httpClient.GetRequestAsync($"{typeof(SearchParameter).Name}?_id={ids}");
+#region Bulk Data Operation
 
-//  Cast the response back to a List of type SearchParameter.
-var response = content
-    .ToBundle()
-    .ToFhirResourceWherePosible<SearchParameter>();
-
-//  Calculate missing SearchParameter/s.
-var missingSearchParameters = response
-    .Where(p => ids.Contains(p.Id))
-    .ToList();
-
-//  Write results.
-Console.WriteLine($"Environment: {baseUrl}\n" +
-    $"Custom {typeof(SearchParameter).Name}'s: {searchParameters.Count}\n" +
-    $"Missing {typeof(SearchParameter).Name}'s: {missingSearchParameters.Count}\n\n");
-
-//  Write individual missing SearchParameter/s.
-var i = 1;
-foreach (var searchParameter in missingSearchParameters)
+if(runBulkDataOp == true)
 {
-    Console.WriteLine($"Nbr: {i} Id: {searchParameter.Id}, Code: {searchParameter.Code}, Resource: {string.Join(", ", searchParameter.Base)}");
-    i++;
+    const string PATIENT_ID = "9e1ab93f-ce36-4028-88bb-aff23a433d55";
+    const string SITE_ID = "fadf8070-2b16-412e-b1d6-fad64e4d7afe";
+    var preliminaryStatus = ObservationStatus.Preliminary.ToString().ToLower();
+    var registeredStatus = ObservationStatus.Registered.ToString().ToLower();
+
+    var detetedIssueQuery = $"{typeof(DetectedIssue).Name}?patient={typeof(Patient).Name}/{PATIENT_ID}&status={preliminaryStatus}";
+    var content = await httpClient.GetRequestAsync(detetedIssueQuery);
+    if (string.IsNullOrEmpty(content))
+    {
+        Console.WriteLine("Bulk update DetectedIssue date not found.");
+        return;
+    }
+    var detectedIssues = content
+        .ToBundle()
+        .ToFhirResourceWherePosible<DetectedIssue>();
+    var bundle = new Bundle
+    {
+        Type = Bundle.BundleType.Transaction,
+    };
+    var entries = new List<Bundle.EntryComponent>();
+
+    foreach(var detectedIssue in detectedIssues)
+    {
+        Console.WriteLine($"Id: {detectedIssue.Id}");
+        var fullUrl = $"{httpClient.BaseAddress}/{typeof(DetectedIssue).Name}/{detectedIssue.Id}";
+        
+        //  You could do this however you can't add request.
+        //bundle.AddResourceEntry(detectedIssue, fullUrl);
+
+        var entry = new Bundle.EntryComponent
+        {
+            FullUrl = fullUrl,
+            Request = new Bundle.RequestComponent
+            {
+                Method = Bundle.HTTPVerb.PUT,
+                Url = $"{typeof(DetectedIssue).Name}/{detectedIssue.Id}"
+            },
+            Resource = detectedIssue
+        };
+        entries.Add(entry);
+    }
+
+    //  add to HttpClient to persist
 }
+
+#endregion
+#region SearchParameter.
+
+if (runSearchParams == true)
+{
+    //  Read the JSON file for all required custom Search Parameters to a bundle. 
+    var bundle = await path.ReadDataFileAsync();
+
+    //  Cast to a List of type SearchParameter.
+    var searchParameters = bundle.ToFhirResourceWherePosible<SearchParameter>();
+
+    //  Comma delimited string of Id for the Fhir Service query. This is MVP. There may come a time when there are too many and a batching strategy will be required.
+    var ids = string.Join(',', searchParameters
+        .Select(p => p.Id)
+        .ToList());
+
+    
+
+    //  Execute the request.
+    var content = await httpClient.GetRequestAsync($"{typeof(SearchParameter).Name}?_id={ids}");
+
+    //  Cast the response back to a List of type SearchParameter.
+    var response = content
+        .ToBundle()
+        .ToFhirResourceWherePosible<SearchParameter>();
+
+    //  Calculate missing SearchParameter/s.
+    var missingSearchParameters = response
+        .Where(p => ids.Contains(p.Id))
+        .ToList();
+
+    //  Write results.
+    Console.WriteLine($"Environment: {baseUrl}\n" +
+        $"Custom {typeof(SearchParameter).Name}'s: {searchParameters.Count}\n" +
+        $"Missing {typeof(SearchParameter).Name}'s: {missingSearchParameters.Count}\n\n");
+
+    //  Write individual missing SearchParameter/s.
+    var i = 1;
+    foreach (var searchParameter in missingSearchParameters)
+    {
+        Console.WriteLine($"Nbr: {i} Id: {searchParameter.Id}, Code: {searchParameter.Code}, Resource: {string.Join(", ", searchParameter.Base)}");
+        i++;
+    }
+}
+
+
+#endregion
 
 //  Done.
 Console.WriteLine("\n\nFhir Utility completed");
